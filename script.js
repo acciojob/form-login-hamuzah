bbfb454d6aaffb2d55fb6d224eb11e074106db36
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-28 16:18:30 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function getFormvalue() {
     //Write your code here
-	  //event.preventDefault();  // Prevent the form from submitting the traditional way
-    const form = document.getElementById("form1");
-    const fname = form.elements["fname"].value;  // Get the value of the first name input field
-    const lname = form.elements["lname"].value;  // Get the value of the last name input field
-	
-     console.log(alert(fname + " " + lname));
+	  let first = document.getElementById("first").value;
+	let last = document.getElementById("last").value;
+	alert(first+" "+last);
 
 } )
 
