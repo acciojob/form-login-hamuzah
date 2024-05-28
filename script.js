58874a9e635dce274b89522bf207d9576ddc6d2c
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-28 16:37:02 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function getFormvalue() {
     //Write your code here
-	  let first = document.getElementById("first").value;
-	let last = document.getElementById("last").value;
-	alert(first+" "+last);
+	 document.getElementById('form1').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent the form from submitting the traditional way
+    const fname = document.getElementById('fname').value;  // Get the value of the first name input field
+    const lname = document.getElementById('lname').value;  // Get the value of the last name input field
+    alert(fname + " " + lname);  // Display the first and last name using alert()
+});  
 
-} )
+ 
 
 }
